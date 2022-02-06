743e52fa4a44a99a0a190d7a564b0a03779463ea
crinfray <crinfray@protonmail.com>
2022-02-05 20:47:34 -0400

    dependencies: Add logback

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(project(":kafka"))
     implementation("no.tornado:tornadofx:1.7.20")
     runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.6.0")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.10")
 }
 
 javafx {
